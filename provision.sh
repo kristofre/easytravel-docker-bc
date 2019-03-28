af2baa64381ffea79a87a79aa16ae52e7c4d5b5c
kristofre <kristof.renders@dynatrace.com>
2019-03-28 17:08:07 +0100

    Update provision.sh
    added -y

--- provision.sh
+++ provision.sh
@@ -6,7 +6,7 @@ DT_DOWNLOAD_URI=""
 # Install docker and docker-compose
 #echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
